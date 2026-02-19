b79c0b7ef99c5031a1b4eae2a3cc4fbe11d47a56
SManoj <savaramanoj2006@gmail.com>
2026-02-19 23:00:24 +0530

    made changes in sessionController

--- backend/src/controllers/sessionController.js
+++ backend/src/controllers/sessionController.js
@@ -98,6 +98,13 @@ export async function joinSession(req,res) {
         if(!session ){
             return res.status(404).json({message:"Session not found"})
         }
+        if (session.status !== "active") {
+            return res.status(400).json({ message: "Cannot join a completed session" });
+        }
+
+        if (session.host.toString() === userId.toString()) {
+            return res.status(400).json({ message: "Host cannot join their own session as participant" });
+        }
          if(session.participant){
             return res.status(404).json({message:"Session is full"})
         }
